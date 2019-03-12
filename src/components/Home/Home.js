a9d61098df5672e8f66bd2b8012912d9d3a1e6f1
Tony Toussaint <tonyyasi@gmail.com>
2019-03-12 06:26:57 -0600

    Modifies link to go to /catalog

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ export default class Home extends React.Component {
 
 
     handleOrder = () => {
-        customHistory.push('/catalogo');
+        customHistory.push('/catalog');
     }
 
     render() {
