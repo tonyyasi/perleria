0b5dc3b8203eaa6d2bffe4d470056c5166b4ca8d
Tony Toussaint <tonyyasi@gmail.com>
2019-04-08 17:55:38 -0500

    Use bootstrap modal to have good styles

--- src/components/CommonModal/CommonModal.js
+++ src/components/CommonModal/CommonModal.js
@@ -1,28 +1,24 @@
 import * as React from 'react';
-import Modal from 'react-modal';
-
-
-const customStyles = {
-    content: { top: '10%', left: '40%', right: 'auto', bottom: 'auto' },
-  };
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export default class CommonModal extends React.Component {
 
     render() {
         const {shown, closeModal, title, subtitle, buttonText, onClick} = this.props;
         return (
-            <Modal
-          isOpen={shown}
-          onRequestClose={closeModal}
-          contentLabel="Selected option"
-          closeTimeoutMS={200}
-          style={customStyles}
-        >
-        <h2>{title}</h2>
-        <p>{subtitle}</p>
-        <button onClick={onClick}>{buttonText}</button>
-        
+          <Modal show={shown} onHide={closeModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>{title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{subtitle}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={onClick}>
+              {buttonText}
+            </Button>
+          </Modal.Footer>
         </Modal>
+       
         )
     }
 }
