7996e231101fa199e69ef21336b1a947d36af379
Tony Toussaint <tonyyasi@gmail.com>
2019-05-07 17:29:41 -0500

    Complete flow of making orders

--- src/components/Paypal/PaypalComponent.js
+++ src/components/Paypal/PaypalComponent.js
@@ -12,15 +12,6 @@ const ENV = process.env.NODE_ENV === 'production'
 
 class PaypalComponent extends React.Component {
   render() {
-    const onSuccess = (payment) =>
-      console.log('Successful payment!', payment);
-
-    const onError = (error) =>
-      console.log('Erroneous payment OR failed to load script!', error);
-
-    const onCancel = (data) =>
-      console.log('Cancelled payment!', data);
-
     return (
       <div>
         <PaypalButton
@@ -29,9 +20,9 @@ class PaypalComponent extends React.Component {
           commit={true}
           currency={'MXN'}
           total={this.props.totalPrice}
-          onSuccess={onSuccess}
-          onError={onError}
-          onCancel={onCancel}
+          onSuccess={this.props.onSuccess}
+          onError={this.props.onError}
+          onCancel={this.props.onCancel}
         />
       </div>
     );
